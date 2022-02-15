9af17dfadc7f3b847d0d98d14349d24651f2c179
Anthony Miano <anthonymiano06@gmail.com>
2022-02-15 11:34:37 +0300

    Task2 code with comments for task1 code

--- setup.sh
+++ setup.sh
@@ -1,6 +1,22 @@
 #!bin/bash
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install postgresql postgresql-contrib -y
-sudo -u postgres psal - "create database mob_db;"
-sudo -u postgres psql -c "create user mob_db_user; alter user mob_b_user with encrypted password "mob_db_password"; GRANT ALL PRIVILEGES ON DATABASE mob db TO mob_db_user;”
 
+#Getting updates and upgrading the server
+sudo apt-get update -y  && sudo apt-get upgrade -y
+
+#installing postgresql
+sudo apt-get install postgresql postgresql-contrib -y
+
+
+#Creating database
+sudo -u postgres psql -c "create database mob_db;"
+
+#Creating user
+sudo -u postgres psql -c "create user mob_db_user; alter user mob_db_user with encrypted password 'mob_db_password'; grant all privileges on database mob_db to mob_db_user"
+
+#Adding user named "mob_app_user"
+sudo adduser mob_app_user
+
+#Enter user's password and credentials
+
+#Installing home directory for "mob_app_user"
+sudo mkhomedir_helper mob_app_user
